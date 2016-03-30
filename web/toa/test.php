2513a334d887ec12fc74173a4240fc200133e9b8
Hans Winzell <hans.winzell@gmail.com>
2016-03-30 12:04:08 +0200

    Delete test.php

--- web/toa/test.php
+++
@@ -1,6 +0,0 @@
-<?php
-$objDateTime = new DateTime('NOW');
-echo date('Y-m-d H:i:s');
-//echo $objDateTime->format('c'); // ISO8601 formated datetime
-//echo $objDateTime->format(DateTime::ISO8601); // Another way to get an ISO8601 formatted string
-?>
